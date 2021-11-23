60fcaf02909695e1ad506804a202325f9de4edd2
DocLiam <51264013+DocLiam@users.noreply.github.com>
2021-11-23 22:22:42 +0200

    update5

--- C/Main Learner/DATA/crypto.py
+++ C/Main Learner/DATA/crypto.py
@@ -1,7 +1,7 @@
 from decimal import *
 from random import *
 
-k1 = Decimal(1.3949867209205726765)
+k1 = Decimal(0.3949867209205726765)
 k2 = Decimal(0.2834739513647389458)
 k3 = Decimal(0.3829249028428461492)
 k4 = Decimal(0.4856829189385472123)
@@ -10,22 +10,22 @@ filew = open("WATCRYPTTRAIN.txt", "w")
 
 to_write = ""
 
-for i in range(1000):
-    p1 = Decimal(random())
-    o1 = k1*((p1+k1)**k1)+k1
-
-    p2 = Decimal(random())
-    o2 = k2*((o1+p2+k2)**k2)+k2
-
-    p3 = Decimal(random())
-    o3 = k3*((o2+p3+k3)**k3)+k3
-
-    p4 = Decimal(random())
-    o4 = k4*((o3+p4+k4)**k4)+k4
-    
+for i in range(100):
     k0 = Decimal(random())
+    
+    for i in range(50):
+        p1 = Decimal(random())
+        o1 = k1*((p1*k0+k1)**k1)+k1
 
-    to_write += str(p1) + "," + str(k0) + ":" + str(o1*k0) + "\n"
+        to_write += str(p1) + "," + str(k0) + ":" + str(o1) + "\n"
+    
+    p1 = Decimal(random())
+    
+    for i in range(50):
+        k0 = Decimal(random())
+        o1 = k1*((p1*k0+k1)**k1)+k1
+
+        to_write += str(p1) + "," + str(k0) + ":" + str(o1) + "\n"
 
 filew.write(to_write[:-1])
 filew.close()
@@ -34,22 +34,22 @@ filew = open("WATCRYPTVALIDATE.txt", "w")
 
 to_write = ""
 
-for i in range(1000):
-    p1 = Decimal(random())
-    o1 = k1*((p1+k1)**k1)+k1
-
-    p2 = Decimal(random())
-    o2 = k2*((o1+p2+k2)**k2)+k2
-
-    p3 = Decimal(random())
-    o3 = k3*((o2+p3+k3)**k3)+k3
-
-    p4 = Decimal(random())
-    o4 = k4*((o3+p4+k4)**k4)+k4
-    
+for i in range(100):
     k0 = Decimal(random())
+    
+    for i in range(50):
+        p1 = Decimal(random())
+        o1 = k1*((p1*k0+k1)**k1)+k1
 
-    to_write += str(p1) + "," + str(k0) + ":" + str(o1*k0) + "\n"
+        to_write += str(p1) + "," + str(k0) + ":" + str(o1) + "\n"
+    
+    p1 = Decimal(random())
+    
+    for i in range(50):
+        k0 = Decimal(random())
+        o1 = k1*((p1*k0+k1)**k1)+k1
+
+        to_write += str(p1) + "," + str(k0) + ":" + str(o1) + "\n"
 
 filew.write(to_write[:-1])
 filew.close()
@@ -58,22 +58,22 @@ filew = open("WATCRYPTTEST.txt", "w")
 
 to_write = ""
 
-for i in range(1000):
-    p1 = Decimal(random())
-    o1 = k1*((p1+k1)**k1)+k1
-
-    p2 = Decimal(random())
-    o2 = k2*((o1+p2+k2)**k2)+k2
-
-    p3 = Decimal(random())
-    o3 = k3*((o2+p3+k3)**k3)+k3
-
-    p4 = Decimal(random())
-    o4 = k4*((o3+p4+k4)**k4)+k4
-    
+for i in range(100):
     k0 = Decimal(random())
+    
+    for i in range(50):
+        p1 = Decimal(random())
+        o1 = k1*((p1*k0+k1)**k1)+k1
 
-    to_write += str(p1) + "," + str(k0) + ":" + str(o1*k0) + "\n"
+        to_write += str(p1) + "," + str(k0) + ":" + str(o1) + "\n"
+    
+    p1 = Decimal(random())
+    
+    for i in range(50):
+        k0 = Decimal(random())
+        o1 = k1*((p1*k0+k1)**k1)+k1
+
+        to_write += str(p1) + "," + str(k0) + ":" + str(o1) + "\n"
 
 filew.write(to_write[:-1])
 filew.close()
--- C/Main Learner/Training.py
+++ C/Main Learner/Training.py
@@ -4,10 +4,10 @@ data_name = input("Data name: ")
 model_name = input("Model name: ")
 
 Model0 = Model()
-Model0.load(model_name, min_diff=0.0003, learning_rate=0.0001, cycles=-1, hidden_shaped=True, normaliser_depth=0)
+Model0.load(model_name, min_diff=0.0003, learning_rate=0.001, cycles=1000, hidden_shaped=True, normaliser_depth=0)
 
 Data0 = Data(Model0.input_count)
 Data0.extractall(data_name)
 
 Model0.train(Data0)
-Model0.save()hello
+Model0.save()
