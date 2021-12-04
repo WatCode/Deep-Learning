974edef14c2d33344026c5d963106e592c2a7fc5
Liam <51264013+DocLiam@users.noreply.github.com>
2021-12-04 16:01:21 +0200

    update

--- C/Main Learner/Testing.py
+++ C/Main Learner/Testing.py
@@ -13,6 +13,7 @@ Data0.extractall(data_name)
 Model0.test(Data0)
 
 print(Model0.output_values)
+
 new_output_values = []
 avg_value = sum(Model0.output_values)/Decimal(len(Model0.output_values))
 
@@ -25,9 +26,10 @@ for i in range(len(Model0.output_values)):
     else:
         new_output_values.append(Decimal(0))
 
-    total += 1
-        
-    if new_output_values[i] == Data0.target_values_test[i]:
-        count += 1
+    if Data0.target_values_test[i] == Decimal(1):
+        total += 1
+            
+        if new_output_values[i] == Data0.target_values_test[i]:
+            count += 1
     
 print(count/total)
