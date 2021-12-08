c50869db8f33b3e17cbfceddfca45c2c7b1ed66b
DocLiam <51264013+DocLiam@users.noreply.github.com>
2021-12-08 14:00:08 +0200

    update

--- C/Main Learner/DATA/covdiffprop.py
+++ C/Main Learner/DATA/covdiffprop.py
@@ -8,11 +8,10 @@ heading_list = filer[0].split(",")
 
 heading_data = {heading:[] for heading in heading_list}
 
-length = -1
+length = 0
 
 for line in filer[1:]:
     line_split = line.split(",")
-    
     length += 1
     
     for i in range(len(line_split)):
@@ -29,32 +28,19 @@ target_headings = ["EC","FS","GP","KZN","LP","MP","NC","NW","WC"]
 
 heading_size = {"EC":6.712276,"FS":2.887465,"GP":15.176115,"KZN":11.289086,"LP":5.982584,"MP":4.592187,"NC":1.263875,"NW":4.027160,"WC":6.844272}
 
+average_size = 7
+
 normalised_data = {heading:[] for heading in target_headings}
-
-for heading in target_headings:
-    for i in range(len(heading_data[heading])-1):
-        normalised_data[heading].append((heading_data[heading][i+1]-heading_data[heading][i]))
-            
-moving_average_data = {heading:[] for heading in target_headings}
 sum_data = [Decimal(0) for i in range(length)]
-average_length = 7
+
+for heading in target_headings:
+    for i in range(len(heading_data[heading])):
+        normalised_data[heading].append(heading_data[heading][i])
+        sum_data[i] += heading_data[heading][i]
 
 for heading in target_headings:
     for i in range(len(normalised_data[heading])):
-        upper = i+average_length
-        
-        if upper > len(normalised_data[heading]):
-            upper = len(normalised_data[heading])
-            
-        moving_average_data[heading].append(sum(normalised_data[heading][i:upper])/Decimal(upper-i))
-        sum_data[i] += moving_average_data[heading][i]
-
-for heading in target_headings:
-    for i in range(len(moving_average_data[heading])):
-        moving_average_data[heading][i] /= sum_data[i]
-
-for heading in target_headings:
-    for i in range(len(normalised_data[heading])):
+        normalised_data[heading][i] /= sum_data[i]
 
 input_size = 180
 output_size = 7
@@ -67,10 +53,10 @@ for heading in target_headings:
     for i in range(length-input_size-output_size):
         for j in range(input_size):
             if i%2 == 0:
-                to_write_train += str(moving_average_data[heading][i+j]) + ","
+                to_write_train += str(normalised_data[heading][i+j]) + ","
             else:
-                to_write_validate += str(moving_average_data[heading][i+j]) + ","
-            to_write_test += str(moving_average_data[heading][i+j]) + ","
+                to_write_validate += str(normalised_data[heading][i+j]) + ","
+            to_write_test += str(normalised_data[heading][i+j]) + ","
 
         if i%2 == 0:
             to_write_train = to_write_train[:-1] + ":"
@@ -80,10 +66,10 @@ for heading in target_headings:
 
         for j in range(output_size):
             if i%2 == 0:
-                to_write_train += str(moving_average_data[heading][i+input_size+j]) + ","
+                to_write_train += str(normalised_data[heading][i+input_size+j]) + ","
             else:
-                to_write_validate += str(moving_average_data[heading][i+input_size+j]) + ","
-            to_write_test += str(moving_average_data[heading][i+input_size+j]) + ","
+                to_write_validate += str(normalised_data[heading][i+input_size+j]) + ","
+            to_write_test += str(normalised_data[heading][i+input_size+j]) + ","
         
         if i%2 == 0:
             to_write_train = to_write_train[:-1] + "\n"
