a47dcf0827e506bedc10e2f6fc6ef97bc14e4503
Liam <51264013+DocLiam@users.noreply.github.com>
2021-12-14 16:16:20 +0200

    update

--- Main Learner/Testing LONGCOV.py
+++ Main Learner/Testing LONGCOV.py
@@ -15,7 +15,7 @@ Model0.test(Data0)
 print(Model0.output_values)
 
 new_output_values = []
-avg_value = sorted(Model0.output_values)[int(len(Model0.output_values)/2)]
+avg_value = sum(Model0.output_values)/Decimal(len(Model0.output_values))
 
 count = 0
 total = 0
@@ -26,10 +26,9 @@ for i in range(len(Model0.output_values)):
     else:
         new_output_values.append(Decimal(0))
 
-    if Data0.target_values_test[i] == Decimal(1):
-        total += 1
-            
-        if new_output_values[i] == Data0.target_values_test[i]:
-            count += 1
+    total += 1
+        
+    if new_output_values[i] == Data0.target_values_test[i]:
+        count += 1
     
 print(count/total)
