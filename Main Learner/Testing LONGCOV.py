efd52eefba4b5df337f8e50fcce33fbaa14398aa
DocLiam <51264013+DocLiam@users.noreply.github.com>
2021-12-14 19:24:56 +0200

    update

--- Main Learner/Testing LONGCOV.py
+++ Main Learner/Testing LONGCOV.py
@@ -13,13 +13,15 @@ Data0.extractall(data_name)
 
 Model0.test(Data0)
 
+comorbidity_count = 24
+
 symptom_count = 12
 
 symptom_names = ['Shortness of Breath', 'Recurring Chest Pain', 'Low Oxygen Levels', 'Heart Rate Dysfunction', 'Constant Fatigue', 'Joint and Muscle Pain', 'Brain Fog', 'Loss of Smell/Taste', 'Sleep Disturbances', 'Depression/Anxiety', 'Digestive Problems', 'Kidney Problems']
 
 symptom_target_values = [[Data0.target_values_test[j*symptom_count+i] for j in range(int(len(Data0.target_values_test)/symptom_count))] for i in range(symptom_count)]
 
-symptom_output_values = [[Model0.output_values[j*symptom_count+i] for j in range(int(len(Model0.output_values)/symptom_count))] for i in range(symptom_count)]
+symptom_output_values = [sorted([Model0.output_values[j*symptom_count+i]*((sum(Data0.target_values_test[j*symptom_count:j*symptom_count+symptom_count])/sum(Model0.output_values[j*symptom_count:j*symptom_count+symptom_count]))**Decimal(10)) for j in range(int(len(Model0.output_values)/symptom_count))]) for i in range(symptom_count)]
 
 symptom_minimum_threshold_vector = []
 symptom_maximum_threshold_vector = []
@@ -37,12 +39,11 @@ for i in range(symptom_count):
     
     symptom_minimum_threshold_vector.append(minimum)
     symptom_maximum_threshold_vector.append(maximum)
-    symptom_average_threshold_vector.append(sum(symptom_output_values[i])/len(symptom_output_values[i]))
+    symptom_average_threshold_vector.append(sum(symptom_target_values[i])/len(symptom_target_values[i]))
     
 print(symptom_minimum_threshold_vector)
 print(symptom_maximum_threshold_vector)
-
-proportion = sum(Model0.output_values)/len(Model0.output_values)
+print(symptom_average_threshold_vector)
 
 new_output_values = []
 
@@ -55,16 +56,10 @@ total_negative = 0
 for i in range(len(Model0.output_values)):
     symptom_num = i%symptom_count
     
-    if symptom_minimum_threshold_vector[symptom_num] >= symptom_maximum_threshold_vector[symptom_num]:
-        if Model0.output_values[i] >= symptom_minimum_threshold_vector[symptom_num]:
-            new_output_values.append(Decimal(1))
-        else:
-            new_output_values.append(Decimal(0))
+    if Model0.output_values[i] >= symptom_output_values[symptom_num][-int(Decimal(1.3)*symptom_average_threshold_vector[symptom_num]*len(symptom_output_values[symptom_num]))]:
+        new_output_values.append(Decimal(1))
     else:
-        if Model0.output_values[i] <= symptom_minimum_threshold_vector[symptom_num]+(symptom_maximum_threshold_vector[symptom_num]-symptom_minimum_threshold_vector[symptom_num])*proportion:
-            new_output_values.append(Decimal(1))
-        else:
-            new_output_values.append(Decimal(0))
+        new_output_values.append(Decimal(0))
     
     if Data0.target_values_test[i] == Decimal(1):
         if new_output_values[i] == Data0.target_values_test[i]:
